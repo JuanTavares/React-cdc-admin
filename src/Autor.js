c9af59d70a6972b22c63e32f1ff843e89d769fe6
Juan Carlos <juantavares2006@hotmail.com>
2018-05-15 22:47:06 -0300

    Criando os componentes que serão utilizados

---
+++ src/Autor.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado';
+import PubSub from 'pubsub-js';
+
+class FormularioAutor extends Component {
+
+    render() {
+
+        return;
+    }
+}
+
+class TabelaAutores extends Component {
+
+    render() {
+
+        return;
+    }
+}
+
+export default class AutorBox extends Component {
+
+    render() {
+
+        return;
+    }
+}
