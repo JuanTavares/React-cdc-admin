b9cadef79f59398affb060892ab637ab561f63f4
Juan Carlos <juantavares2006@hotmail.com>
2018-05-16 22:08:21 -0300

    Declarando as rotas na index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import AutorBox from './Autor';
+import Home from './Home';
+import Livro from './Livro';
+import './index.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <Router>
+        <App>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/autor" component={AutorBox} />
+                <Route path="/livro" component={Livro} />
+            </Switch>
+        </App>
+    </Router>)
+);
 registerServiceWorker();
